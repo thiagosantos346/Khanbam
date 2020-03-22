e75e847a7bf8679947b2f095fc789e2929481bea
silva.thiago <silva.thiago@totvs.com.br>
2020-03-21 21:59:38 -0300

    initial commit;

---
+++ src/components/Board/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+// & + div toda div que tem uma div antes dela, a primeira não deve ter;
+export const Container = styled.div`
+    display : flex;
+    padding : 30px;
+    height : calc(100% - 80px);
+`;
